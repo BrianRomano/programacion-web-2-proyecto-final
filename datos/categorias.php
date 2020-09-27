023ed45bc4f4440f6f64e62b0a8eadc23c14ca88
Brian Romano <brianromanook@gmail.com>
2020-09-27 19:55:20 -0300

    Datos en proceso

---
+++ datos/categorias.php
@@ -0,0 +1,20 @@
+<?php 
+    $categoria = array(
+        1 => array(
+            'id_categoria' => 1,
+            'nombre' => 'Cuero'
+        ),
+        2 => array(
+            'id_categoria' => 2,
+            'nombre' => 'Dibujos'
+        ),
+        3 => array(
+            'id_categoria' => 3,
+            'nombre' => 'Madera'
+        ),
+        4 => array(
+            'id_categoria' => 4,
+            'nombre' => 'Silicona'
+        )
+    );
+?>
