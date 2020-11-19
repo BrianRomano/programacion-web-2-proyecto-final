6c8d054c441b1ac30e68e06157c191de2850c9fc
Brian Romano <brianromanook@gmail.com>
2020-11-19 18:47:17 -0300

    Formulario en proceso

--- admin/agregar-productos.php
+++ admin/agregar-productos.php
@@ -25,12 +25,26 @@
   $datosJson = json_decode($datos,true);
 
   if(isset($_POST['add'])){
+
+    if(isset($_FILES['imagen'])){
+      $archivo = $_FILES["imagen"];
+      $nombre = $archivo['name'];
+      $tipo = $archivo["type"];
+
+      //Condición de tipo de archivo
+      if($tipo == "image/jpg" || $tipo == "image/jpeg" || $tipo == "image/png" || $tipo == "image/gif"){
+        move_uploaded_file($archivo['tmp_name'], '../imagenes/fundas/'.$nombre);
+      }
+    }
+
     if(isset($_GET['edit'])){
         $id = $_GET['edit'];
     }else{
         $id = date('Ymdhis');
     }
-  $datosJson[$id] = array('id_producto'=>$id, 'nombre'=>$_POST['nombre'], 'precio'=>$_POST['precio'], 'imagen'=>$_POST['imagen'], 'descripcion'=>$_POST['descripcion'], 'activo'=>$_POST['activo'], 'destacado'=>$_POST['destacado'], 'id_categoria'=>$_POST['id_categoria'], 'id_marca'=>$_POST['id_marca']);
+
+
+  $datosJson[$id] = array('id_producto'=>$id, 'nombre'=>$_POST['nombre'], 'precio'=>$_POST['precio'], 'imagen'=>$_FILES['imagen']['name'], 'descripcion'=>$_POST['descripcion'], 'activo'=>$_POST['activo'], 'destacado'=>$_POST['destacado'], 'id_categoria'=>$_POST['id_categoria'], 'id_marca'=>$_POST['id_marca']);
   $fp = fopen('../datos/producto.json','w');
   $datosString = json_encode($datosJson);
   fwrite($fp,$datosString);
@@ -62,16 +76,46 @@
 </head>
 
 <body class="dark-edition">
-    <div id="login" class = "agregar">
-    <form action="" method="post">
+    <div id="login" class = "agregar agregarProducto">
+    <form action="" method="post" enctype="multipart/form-data">
           <div class="form-group">
             <label for="exampleDropdownFormEmail1" name="nombreMaterial" class ="titulo">Nuevo producto</label>
             <input type="text" placeholder="Nombre" name="nombre" class="form-control" value="<?php echo isset($dato)?$dato['nombre']:''?>">
             <input type="text" placeholder="Precio" name="precio" class="form-control" value="<?php echo isset($dato)?$dato['precio']:''?>">
             <input type="text" placeholder="Descripción" name="descripcion" class="form-control" value="<?php echo isset($dato)?$dato['descripcion']:''?>">
-            <input type="submit" class="btn btn-primary loginBtn" name="add" value="Agregar">
+            <select class="form-control">
+            <?php 
+              $datos = file_get_contents("../datos/categoria.json");
+              $datosJson = json_decode($datos, true);
+              foreach($datosJson as $cat):
+            ?>
+              <option class="opciones"><?php echo $cat['nombre']?></option>
+            <?php 
+              endforeach;
+            ?>
+            </select>
+            <select class="form-control">
+            <?php 
+              $datos = file_get_contents("../datos/marca.json");
+              $datosJson = json_decode($datos, true);
+              foreach($datosJson as $marc):
+            ?>
+              <option class="opciones"><?php echo $marc['nombre']?></option>
+            <?php 
+              endforeach;
+            ?>
+            </select>
+            <div id="check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Activo</label><br>
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Destacado</label>
+            </div>
+          <div class="form-group">
+            <input type="file" name="imagen" class="form-control-file" id="exampleFormControlFile1">
           </div>
-      </form>
+      <input type="submit" class="btn btn-primary loginBtn" name="add" value="Agregar">
+    </form>
     </div>
 </body>
 </html>
